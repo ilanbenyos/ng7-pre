aafa047483bf76e37945d9aa6e4b346b2cfa1a93
ilanbenyos <ilan_ben@hotmal.com>
2019-06-13 08:05:50 +0300

    wip

--- src/app/blocks/block-header/block-header.component.ts
+++ src/app/blocks/block-header/block-header.component.ts
@@ -4,7 +4,7 @@ import {Component, Input, OnInit} from '@angular/core';
   selector: 'app-block-header',
   template: `
     <div class="header p-3 d-flex justify-content-between block-border bg-white">
-      <div class="font-sm font-weight-bold">{{title}}</div>
+      <div class="font-sm font-weight-bold d-flex align-items-center">{{title}}</div>
       <div class="counts d-flex">
         <div class="success-avatar avatar avatar-small bg-primary" *ngIf="successCount">{{successCount}}</div>
         <div class="error-avatar avatar avatar-small ml-2 bg-danger" *ngIf="errorCount">{{errorCount}}</div>
--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit,  EventEmitter, Input, Output  } from '@angular/core'
 @Component({
   selector: 'app-nav',
   template:`
-    <div class="nav-app position-fixed">
+    <div class="nav-app position-fixed z-index-2">
       <div class="overlay" 
            (click)="toggleNav()" 
            [ngClass]="isNav ? 'is-nav': ''">{{isNav}}
